54e5edb6804a87fc3ea85e3779fe142720703f1d
hclasmn <32213103+hclasmn@users.noreply.github.com>
2020-10-16 08:46:30 +0800

    Update secret.py

--- secret.py
+++ secret.py
@@ -5,12 +5,12 @@ IS_USE_WX_PUSH = False
 FT_QQ_KEY = ''
 
 ### 是否启动qq推送  请 前往 https://wx.scjtqs.com/qq/user 配置qq并获取token
-IS_USE_QQ_PUSH = False
+IS_USE_QQ_PUSH = os.environ["PUSH_ON"]
 
 PUSH_QQ = 0   ###### 个人qq推送
 
-PUSH_GROUP = 0  ###### qq群推送
+PUSH_GROUP = os.environ["PUSH_GROUP"]  ###### qq群推送
 
-PUSH_TOKEN = ''   ###### qq推送秘钥
+PUSH_TOKEN = os.environ["PUSH_TOKEN"]   ###### qq推送秘钥
 
 
